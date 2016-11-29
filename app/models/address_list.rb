125566ba8a0079b4fdf2bf22cd8916fa472d8c80
linhdang <dangduylinh.bk@gmail.com>
2016-11-29 20:55:06 +0700

    update user

--- app/models/address_list.rb
+++ app/models/address_list.rb
@@ -2,10 +2,10 @@ class AddressList < ApplicationRecord
   belongs_to :ward
   belongs_to :user
 
-  def self.to_s
+  def to_string
   	w = Ward.find_by_id(self.ward_id)
   	d = District.find_by_id(w.district_id)
-  	r = Province.find_by_id(d.Province_id)
-  	s = self.address + w.type + w.name + d.type + d.name + r.type + r.name
+  	r = Province.find_by_id(d.province_id)
+  	s = self.address + ', ' + w.unit + ' ' + w.name + ', ' + d.unit + ' ' + d.name + ', ' + r.unit + ' ' + r.name
   end
 end
