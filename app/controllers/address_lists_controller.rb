eceeec7d017be6959ca18d6cba631ef962f702d6
linhdang <dangduylinh.bk@gmail.com>
2016-11-29 20:47:01 +0700

    update user

--- app/controllers/address_lists_controller.rb
+++ app/controllers/address_lists_controller.rb
@@ -8,10 +8,17 @@ class AddressListsController < ApplicationController
   
   def index
     @user = current_user
+    @address_lists = @user.address_lists.paginate(page: params[:page])
   end
 
   def create
-
+    @user = current_user
+    @address = AddressList.new(address_params)
+    if @address.save
+      redirect_to current_user
+    else
+      redirect_to new_address_list_path
+    end
   end
 
   def edit
@@ -24,8 +31,16 @@ class AddressListsController < ApplicationController
   def destroy
   end
 
+  def filter_districts_by_province
+    @filtered_districts = District.all.where(province_id: params[:selected_province])
+  end
+
+  def filter_wards_by_district
+    @filtered_wards = Ward.all.where(district_id: params[:selected_district])
+  end
+
   private
     def address_params
-      params.require(:address_list).permit(:address, :phone_number, :ward_id)
+      params.require(:address_list).permit( :name,:user_id,:phone_number, :address,:ward_id  )
     end
 end
--- app/models/address_list.rb
+++ app/models/address_list.rb
@@ -1,4 +1,11 @@
 class AddressList < ApplicationRecord
   belongs_to :ward
   belongs_to :user
+
+  def self.to_s
+  	w = Ward.find_by_id(self.ward_id)
+  	d = District.find_by_id(w.district_id)
+  	r = Province.find_by_id(d.Province_id)
+  	s = self.address + w.type + w.name + d.type + d.name + r.type + r.name
+  end
 end
